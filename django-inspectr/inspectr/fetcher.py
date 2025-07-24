f01c992d761315713282b2acb4ed1377706fbf95
richie <richiepagard@gmail.com>
2025-07-25 01:46:33 +0330

    doc: Write doc-stirng for the Dynamic Model Fetcher class and implement `__init__` method.

---
+++ django-inspectr/inspectr/fetcher.py
@@ -0,0 +1,35 @@
+class DynamicModelFetcher:
+    """
+    A utility for dynamically querying Django models based on structured payload input.
+
+    I built this class to collect useful scripts I wrote during my work with Django REST Framework (DRF) projects, 
+    especially for extracting serializer data more efficiently.
+
+    Since I enjoy working low-level, I have a habit of writing custom utility methods to improve performance and structure.
+    This class includes methods for querying over relational models based on a client-provided payload.
+
+    The payload is a dictionary where each key represents a model name from your project. Each model key maps to another
+    dictionary with two keys:
+        - "instances": A list of primary key IDs to filter the model queryset.
+        - "params": A list of field names to fetch from the related model (for FK, M2M, or O2O relationships).
+
+    Example payload:
+        {
+            "inverter": {
+                "instances": [1, 2],
+                "params": ["i_current", "v_current"]  # Fields from the related model
+            },
+            "battery": {
+                "instances": [5, 6, 7],
+                "params": ["temperature", "voltage"]
+            }
+        }
+
+    Different methods in this class handle different types of relationships (e.g. ForeignKey, ManyToMany, etc.),
+    so the structure of the response may vary depending on which method is used.
+    """
+    DATA = {}
+
+    def __init__(self, payload: dict, model_map: dict):
+        self.payload = payload
+        self.model_map = model_map
